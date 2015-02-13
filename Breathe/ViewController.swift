1672290d10607fa58c1d60546dc9a30635f799bb
Cem Schemel <koralcem@gmail.com>
2015-02-12 20:27:54 -0800

    Creating project

---
+++ Breathe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Breathe
+//
+//  Created by Cem Schemel on 2/12/15.
+//  Copyright (c) 2015 Moonworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
