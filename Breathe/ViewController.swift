22a62de92d108dc3a7117150f30948572cc45f55
Cem Schemel <koralcem@gmail.com>
2015-02-21 12:34:10 -0800

    Remove file header comments

--- Breathe/ViewController.swift
+++ Breathe/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Breathe
-//
-//  Created by Cem Schemel on 2/12/15.
-//  Copyright (c) 2015 Moonworks. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
