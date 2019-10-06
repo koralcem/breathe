145a88b59e34f3fdacef69fead9429c13a40a1d5
Cem Schemel <koralcem@gmail.com>
2019-10-05 21:41:24 -0700

    Get bindings across views

--- Breathe/ContentView.swift
+++ Breathe/ContentView.swift
@@ -1,16 +1,52 @@
 import SwiftUI
 
+struct BreathTimingSettings {
+    var breathIn = 4
+    var pauseAfterBreathIn = 0
+    var breathOut = 5
+    var pauseAfterBreathOut = 0
+}
+
 struct ContentView: View {
+    @State var settings: BreathTimingSettings
+
     var body: some View {
-        VStack {
-            Text("Hello World")
-            Text("Hi too")
+        NavigationView {
+            VStack {
+                Text("Breath in: \(settings.breathIn)")
+                Text("Pause after breath in: \(settings.pauseAfterBreathIn)")
+                Text("Breath out: \(settings.breathOut)")
+                Text("Pause after breath out: \(settings.pauseAfterBreathOut)")
+            }.navigationBarItems(trailing:
+                NavigationLink("Timings", destination: TimingsView(settings: $settings))
+            )
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(settings: BreathTimingSettings())
+    }
+}
+
+struct TimingsView: View {
+    @Binding var settings: BreathTimingSettings
+
+    var body: some View {
+        VStack {
+            Stepper(value: $settings.breathIn, in: 1...10) {
+                Text("Breath in: \(settings.breathIn) seconds")
+            }
+            Stepper(value: $settings.pauseAfterBreathIn, in: 0...10) {
+                Text("Pause after breath in: \(settings.pauseAfterBreathIn) seconds")
+            }
+            Stepper(value: $settings.breathOut, in: 1...10) {
+                Text("Breath out: \(settings.breathOut) seconds")
+            }
+            Stepper(value: $settings.pauseAfterBreathOut, in: 0...10) {
+                Text("Pause after breath out: \(settings.pauseAfterBreathOut) seconds")
+            }
+        }
     }
 }
