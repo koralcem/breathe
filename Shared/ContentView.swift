dcc3b705e49c5adfa949939997de3a77a2e26b63
Cem Schemel <koralcem@gmail.com>
2021-04-26 19:58:17 -0700

    Initial groundwork for the circle

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -1,9 +1,38 @@
 import SwiftUI
 
+enum BreathState {
+    case breathingIn
+    case pauseAfterBreathIn
+    case breathingOut
+    case pauseAfterBreathOut
+
+    init() {
+        self = .breathingIn
+    }
+
+    func next() -> BreathState {
+        switch self {
+        case .breathingIn:
+            return .pauseAfterBreathIn
+        case .pauseAfterBreathIn:
+            return .breathingOut
+        case .breathingOut:
+            return .pauseAfterBreathOut
+        case .pauseAfterBreathOut:
+            return .breathingIn
+        }
+    }
+}
+
 struct ContentView: View {
+    @State private var breathState = BreathState()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Circle()
+        // if state is in or after in, scale large
+        // if state is out or after out, scale small
+
+        // somehow apply animation during in and out, but not during others
     }
 }
 
